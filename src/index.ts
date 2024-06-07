cc0a54d0f05360b8b4304d4c5a7319a5719e6194
Lucky Bassi <lucky@jobdone.app>
2024-06-07 18:57:59 +0200

    feat(csv): add csv sample

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+import { parse } from "csv-parse/sync";
+
+const parseCsv = async (
+  fileName: string
+): Promise<OrganizationUserImport[]> => {
+  // read csv file
+  const csv = fs.readFileSync(fileName, "utf8");
+  const parsedCsv = parse(csv, {
+    columns: true,
+    skip_empty_lines: true,
+  });
+
+  console.log(JSON.stringify(parsedCsv, null, 2));
+
+  return parsedCsv;
+};
+
+const organizationUsersToImport: OrganizationUserImport[] = await parseCsv(
+  fileName
+);
+
 console.log("Hello via Bun!");
